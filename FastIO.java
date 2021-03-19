09f0f410b0614cad72e87476b37ddb41976c2fb7
Samir Sharma <76894659+samirsharma-github@users.noreply.github.com>
2021-03-19 23:47:03 +0530

    Create FastIO.java

---
+++ FastIO.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        OutputWriter out = new OutputWriter(System.out);
+        // Always print a trailing "\n" and close the OutputWriter as shown at the end of your output
+        
+        // example:
+        int in = scan.nextInt();
+        out.print(in + " ");
+        out.print(in + "\n");
+        
+        out.close();
+    }
+
+    // fast input
+    static class Scanner {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public Scanner(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream));
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    String line = reader.readLine();
+                    if (line == null)
+                        return null;
+                    tokenizer = new StringTokenizer(line);
+                } catch (Exception e) {
+                    throw(new RuntimeException());
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() { return Integer.parseInt(next()); }
+        public long nextLong() { return Long.parseLong(next()); }
+        public double nextDouble() { return Double.parseDouble(next()); }
+    }
+
+    // fast output
+    static class OutputWriter {
+        BufferedWriter writer;
+
+        public OutputWriter(OutputStream stream) {
+            writer = new BufferedWriter(new OutputStreamWriter(stream));
+        }
+
+        public void print(int i) throws IOException { writer.write(i); }
+        public void print(String s) throws IOException { writer.write(s); }
+        public void print(char[] c) throws IOException { writer.write(c); }
+        public void close() throws IOException { writer.close(); }
+    }
+
+
+}
+
