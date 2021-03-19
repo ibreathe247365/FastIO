e50a194eaa399446d828de44b79c6621abfce032
Samir Sharma <76894659+samirsharma-github@users.noreply.github.com>
2021-03-19 23:47:35 +0530

    Create FastIO.cpp

---
+++ FastIO.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    // turns off sync with C streams/buffers
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    // use '\n' instead of endl to speed up output
+
+    // code goes here
+    // example:
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+
+    return 0;
+}
